4696145b9b95b89979a522740c8796d77415b52c
Acasiax <yunji9039@naver.com>
2023-10-05 00:34:45 +0900

    안내사항을 추가했습니다.

---
+++ Model/MYInformation.swift
@@ -0,0 +1,62 @@
+//
+//  MYInformation.swift
+//  HBDLaunch
+//
+//  Created by 이윤지 on 10/5/23.
+//
+
+//import SwiftUI
+//안내사항
+// ✅ 다운 받으면
+//에셋에 있는 사진들 중에 노란색 경고등이 뜨는 게 있으면 1x 크기로 옮겨두기.
+
+// ✅ 제작자(나)는 현재 커밋 푸쉬에 문제가 있어서 그룹화 하지 못한 상태로 일단 깃허브에 올렸는데, (며칠뒤에 컴터 포멧하여 고칠 예정) 하여튼, 그래서 그룹화 되어있는 목록을 나열해보겠습니다.
+
+//✅ 원작 그룹화 상태
+// 1. Model
+//1-1 : NetworkManager
+//1-2 : DateValue
+//1-3 : Task
+//1-4 : Ubuntu
+//1-5 : CheckboxView
+//1-6 :  Holy
+//1-7 : ClupCardData
+
+// 2. View
+//2-1 : ChatView
+//2-2 : VipCardView
+//2-3 : DATETIME
+//2-4 : DATETIME.TRASH
+//2-5 : birthMonth
+//2-6 : PartyView
+//2-7 : LaunchSCreen
+//2-8 : LaunchSCreen (스토리보드)
+//2-9 : cardView
+//2-10 : Home
+//2-11 : CustomDatePicker
+//2-12 : CustomDatePicker22onborn
+//2-13 : HBDView
+//2-14 : HBDView.sup (전부 주석. 삭제해도 됨)
+//2-15 : AddTaskView
+//2-16 : CalBackground
+//2-17 : ClubView
+//2-18 : wheel (전부 주석. 삭제해도 됨)
+
+
+// 3. ElegantTaskApp = 현 HBDLaunchApp
+//3-1: info
+//3-2: 현 HBDLaunchApp
+//3-3: MainContentView
+//3-4: 에셋파일
+//3-5: PersistenceController
+//3-6: 엔티티 설정🔹
+//🔹엔티티 설정의 어트리뷰드는 총 7개. 이름 , 타입 으로 나열하겠습니다.
+//id2 , UUID
+//isCircleShown, Boolean
+//isOn32, Boolean
+//taskDate2, Date
+//time2, Date
+//timestamp2, Date
+//title2, String
+
+
